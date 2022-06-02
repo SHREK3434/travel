fd0575799bf5a1a2e4149e654174b9f4c6d90c93
SHREK344 <otabekisakov809@gmail.com>
2022-06-02 05:45:33 +0300

    added file readme.md

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 5,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+
+    });
+});
